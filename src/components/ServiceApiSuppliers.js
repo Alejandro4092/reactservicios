8aac78876c63948c0d7fa3f6f43578ca5989f51a
Alejandro4092 <acobomarcos@gmail.com>
2025-10-16 16:22:23 +0200

    arreglado

--- src/components/ServiceApiSuppliers.js
+++ src/components/ServiceApiSuppliers.js
@@ -4,63 +4,72 @@ import axios from "axios"
 export default class ServiceApiSuppliers extends Component {
     cajaid = React.createRef();
 
-    url="https://services.odata.org/V4/Northwind/Northwind.svc/Suppliers";
-    state={
-        suppliers:[],
-        supplier:[]
-    }
-    suppliers=[];
+    url = "https://services.odata.org/V4/Northwind/Northwind.svc/Suppliers";
 
-    loadSuppliers=()=>{
+    state = {
+        suppliers: [],
+        supplier: []
+    }
+
+
+
+    loadSuppliers = () => {
         console.log("Cargando Suppliers");
-        axios.get(this.url).then(response=>{
-        console.log(response.data)
-        this.setState({
-            suppliers:response.data.value
-        })
-        })
+        axios.get(this.url).then(response => {
+            console.log(response.data)
+            this.setState({
+                suppliers: response.data.value
+            });
+        });
         console.log("Despues del servicio");
-
     }
-    loadDatos=(event)=>{
-       event.preventDefault();
-        let userid=this.cajaid.current.value;
-        let provedor=this.state.suppliers.find(supplier=>supplier.SupplierID===userid);
+
+    loadDatos = (event) => {
+        event.preventDefault();
+        let userid = this.cajaid.current.value; // ✅ convertir a número
+        let provedor = this.state.suppliers.find(supplier => supplier.SupplierID == userid);
         console.log(userid);
         this.setState({
-            supplier:provedor
-        })
-
-
-      
-
+            supplier: provedor 
+        });
     }
 
-    componentDidMount=()=>{
+    componentDidMount = () => {
         console.log("Creando Componente");
         this.loadSuppliers();
     }
-  render() {
-    return (
-      <div>
-        <h1>ServiceApiSuppliers</h1>
-        
-       <label>Escribe ID de empleado</label>
-       <form onSubmit={this.loadDatos}>
-        <input type='text' ref={this.cajaid}></input>
-        <button>Buscar Supplier</button>
-        </form>
-      
-        {
-            this.state.suppliers.map((suppliers,index)=>{
-                return(<h3 style={{color:"blue"}}>{suppliers.SupplierID} {suppliers.ContactName}</h3>)
 
+    render() {
+        return (
+            <div>
+                <h1>ServiceApiSuppliers</h1>
 
+                <label>Escribe ID de empleado</label>
+                <form onSubmit={this.loadDatos}>
+                    <input type='text' ref={this.cajaid}></input>
+                    <button>Buscar Supplier</button>
+                </form>
 
-            })
-            <h1>{this.state.supplier.ContactName}</h1>
-        }
-      </div>
-    )
-  }
+                {
+                    this.state.suppliers.map((suppliers, index) => {
+                        return (
+                            <h3 key={index} style={{ color: "blue" }}>
+                                {suppliers.SupplierID} {suppliers.ContactName}
+                            </h3>
+                        );
+                    })
+                }
+
+                
+                {this.state.supplier && (
+                  <div>
+                    
+                    <h1>ID: {this.state.supplier.SupplierID}</h1>
+                    <h1>Nombre {this.state.supplier.ContactName}</h1>
+                    </div>
+                    
+                )}
+            </div>
+        );
+    }
 }
